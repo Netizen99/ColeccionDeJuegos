83c27090276654b83702443e12d4234b1e79fdb0
Netizen99 <victoralecg@hotmail.com>
2019-04-25 15:11:37 -0500

    Initial Commit

---
+++ ColeccionDeJuegos/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ColeccionDeJuegos
+//
+//  Created by Victor C. on 25/04/19.
+//  Copyright © 2019 Victor C. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
